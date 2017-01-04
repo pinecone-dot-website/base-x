93f26f612f26492844e925ffe634ddaaa9c7f2bf
Eric Eaglstun <eric@beta.io>
2017-01-04 14:08:17 -0600

    refactor, working tests

--- index.js
+++ index.js
@@ -1,8 +1,48 @@
-'use strict';
+"use strict";
 
-/**
-*
-*/
-module.exports = function( src, srctable, desttable ){
-	return src;
+module.exports = new function(){
+	this.BASE2  = "01";
+	this.BASE8  = "01234567";
+	this.BASE10 = "0123456789";
+	this.BASE16 = "0123456789ABCDEF";
+	this.BASE32 = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	this.BASE62 = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	this.BASE75 = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_.,!=-*(){}[]";
+
+	/**
+	*
+	*	@param string
+	*	@param string
+	*	@param string
+	*	@return
+	*/
+	this.convert = function( src = '', srctable = '', desttable = '' ){
+		src = src.toString();
+
+		var srclen = srctable.length,
+			destlen = desttable.length,
+			val = 0,
+			numlen = src.length;
+
+		// first convert to base 10
+		for( var i = 0; i < numlen; i ++ ){
+			val = val * srclen + srctable.indexOf( src.charAt(i) );
+		}
+		
+		if( val < 0 ){
+			return 0;
+		}
+		
+		// then covert to any base
+		var r = val % destlen;
+		var res = desttable.charAt(r);
+		var q = Math.floor(val / destlen);
+		while( q ){
+			r = q % destlen;
+			q = Math.floor(q / destlen);
+			res = desttable.charAt(r) + res;
+		}
+		
+		return res;
+	}
 };
